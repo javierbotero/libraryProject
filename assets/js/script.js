60625d439f37ed347645061005488936cb702ead
Javier Botero <javierbotero10271@gmail.com>
2020-09-28 18:04:07 -0500

    Add submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,22 +11,47 @@ function addBookToLibrary() {
 
 }
 
+function type(el) {
+  let result = '';
+  if (el === 'pages') {
+    result = 'number';
+  } else if (el === 'read?') {
+    result = 'checkbox';
+  } else if (el === 'submit') {
+    result = 'submit';
+  } else {
+    result = 'text';
+  }
+  return result;
+}
+
+function closeForm() {
+  const form = document.getElementById('form-books');
+  document.body.removeChild(form);
+}
+
 function displayForm() {
-  const div = document.createElement('div');
+  const formBooks = document.createElement('div');
+  formBooks.setAttribute('id', 'form-books');
+  const close = document.createElement('div');
+  close.setAttribute('onclick', 'closeForm()');
+  close.innerHTML = 'X';
+  formBooks.appendChild(close);
   const form = document.createElement('form');
-  const formElements = ['author', 'title', 'pages', 'read?'];
+  const formElements = ['author', 'title', 'pages', 'read?', 'submit'];
   formElements.forEach(el => {
     const labelName = document.createElement('label');
     labelName.setAttribute('for', el);
     labelName.innerHTML = el;
     const inputName = document.createElement('input');
     inputName.setAttribute('id', el);
-    inputName.setAttribute('type', el === 'pages' ? 'number' : el === 'read?' ? 'checkbox' : 'text');
+    inputName.setAttribute('type', type(el));
     inputName.setAttribute('name', el);
     form.appendChild(labelName);
     form.appendChild(inputName);
   });
   form.setAttribute('class', 'form-book');
-  div.appendChild(form);
-  document.body.appendChild(div);
+  formBooks.appendChild(form);
+  document.body.appendChild(formBooks);
 }
+
